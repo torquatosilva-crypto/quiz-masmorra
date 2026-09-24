69c8a3a686e093d3a1fc0e5d5214944ce1f2e4ea
torquatosilva-crypto <torquato.silva@escola.pr.gov.br>
2026-09-24 08:41:08 -0300

    Create perguntas.js

---
+++ js/js/perguntas.js
@@ -0,0 +1,41 @@
+const perguntas = [
+    {
+        enunciado: `Você, o aventureiro ${hélioNome}, chega às margens da perigosa ${localAventura}. Um ancião local avisa que um artefato antigo precisa ser recuperado antes do pôr do sol. Como você decide abordar a entrada da região?`,
+        alternativas: [
+            {
+                texto: "Avança furtivamente pelas sombras, evitando qualquer atenção desnecessária.",
+                afirmacao: "Sua prudência e sigilo permitiram atravessar os primeiros perigos sem ser notado."
+            },
+            {
+                texto: "Empunha sua arma e marcha com coragem diretamente pelo caminho principal.",
+                afirmacao: "Sua bravura impressionou as criaturas locais, abrindo caminho através da força e determinação."
+            }
+        ]
+    },
+    {
+        enunciado: "No meio do caminho, você encontra uma bifurcação. À esquerda, há um túnel escuro com sons sussurrantes. À direita, uma ponte antiga e desgastada sobre um abismo. Qual caminho você escolhe?",
+        alternativas: [
+            {
+                texto: "Entra no túnel escuro confiando em seus sentidos e intuição.",
+                afirmacao: "Ao enfrentar a escuridão dos túneis, você descobriu segredos ocultos e antigos encantamentos."
+            },
+            {
+                texto: "Atravessa a ponte desgastada com cuidado e agilidade.",
+                afirmacao: "Ao cruzar a perigosa ponte, você demonstrou agilidade incrível e superou o medo da queda."
+            }
+        ]
+    },
+    {
+        enunciado: "No centro da ruína principal, você encontra o Guardião do Artefato. Ele exige saber a motivação do seu coração antes de entregar a relíquia. O que você responde?",
+        alternativas: [
+            {
+                texto: " Busco o artefato para proteger meu povo e trazer paz ao reino.",
+                afirmacao: "Sua nobreza de espírito convenceu o Guardião, tornando você um verdadeiro herói lendário."
+            },
+            {
+                texto: "Busco o poder do artefato para me tornar o aventureiro mais forte de todos.",
+                afirmacao: "Seu desejo insaciável por poder impressionou o Guardião, moldando o seu destino como um destemido conquistador."
+            }
+        ]
+    }
+];
